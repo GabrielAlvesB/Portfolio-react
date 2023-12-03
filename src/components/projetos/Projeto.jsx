b02ef9a88cdac55442e50ff28e7c4f3be667deb4
Gabriel Alves <gabrielalveskos@gmail.com>
2023-12-02 22:09:52 -0300

    vercel

--- src/components/projetos/Projeto.jsx
+++ src/components/projetos/Projeto.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import "./Projeto.css"
-
+import Medcenter from "../../../public/projetos/MediCenter/01-principal-medi.png"
 // import { AiFillHtml5 } from "react-icons/ai";
 
-const Projeto = ({imagem, text, titulo, logo1, logo2, logo3, logo4, logo5}) => {
+const Projeto = () => {
   return (
-    <div className='ProjetoCard'>
-        <div className="imagemProjetoCard d-flex justify-content-center align-items-center">
-            <img src={imagem} alt="" />
-        </div>
-        <div className="textoProjetoCard">
-            <div className="tituloProjetoCard">
-                <h3>{titulo}</h3>
-            </div>
-            <div className="escritaProjetoCard">
-                <p>{text}</p>
-            </div>
-            <div className="d-flex justify-content-between p-1 aaaaaaa">
-                <div >{logo1} {logo2} {logo3} {logo4} {logo5}</div>
-                <button className='botao'>Site</button>
+    <div className='ProjetoCard d-flex justify-content-center'>
+        <div className="cardProjeto d-flex justify-content-center">
+            <div className="CardFinal ">
+                <div className="imagemCard">
+                    
+                </div>
+                <div className="textoCard">
+
+                </div>
+                <div className="botoesCard"> 
+                
+                </div>
+
+    
             </div>
         </div>
     </div>
