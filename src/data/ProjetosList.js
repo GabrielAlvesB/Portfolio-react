30b169a6f0a5bf682a54d1214fc5f6ed0fdc3aa5
Gabriel Alves <gabrielalveskos@gmail.com>
2024-03-31 23:39:06 -0300

    Ajuste nas imagens

--- src/data/ProjetosList.js
+++ src/data/ProjetosList.js
@@ -1,15 +1,21 @@
+import MediCenter from "../../public/img/projetos/MediCenter/01.png"
+import Multistep from "../../public/img/projetos/react_multistep_form/01-alt.png"
+import LojaZap from "../../public/img/projetos/Loja_Zap_shadcn_ui/01.png"
+import B7web from "../../public/img/projetos/logos/b7web.png"
+import Hdc from "../../public/img/projetos/logos/HoradeCodar.png"
+
 export const ProjetosList = [
-    {id:0, name: 'MediCenter', img:'../../public/img/projetos/MediCenter/01.png', link:'https://gabrielalvesb.github.io/MediCenter/', logo:'../../public/img/projetos/logos/b7web.png', icones:[
+    {id:0, name: 'MediCenter', img:MediCenter, link:'https://gabrielalvesb.github.io/MediCenter/', logo: B7web, icones:[
         {id:0, nome:'../../public/img/skills/Html.png'},
         {id:1, nome:'../../public/img/skills/Css.png'},
         {id:2, nome:'../../public/img/skills/visual-studio-code.png'}
     ]},
-    {id:1, name: 'React Multistep Form', img:'../../public/img/projetos/react_multistep_form/01-alt.png', link:'https://multistep-form-react-phi.vercel.app/', logo:'../../public/img/projetos/logos/HoradeCodar.png', icones:[
+    {id:1, name: 'React Multistep Form', img:Multistep, link:'https://multistep-form-react-phi.vercel.app/', logo: Hdc, icones:[
         {id:0, nome:'../../public/img/skills/react.png'},
         {id:1, nome:'../../public/img/skills/Css.png'},
         {id:2, nome:'../../public/img/skills/visual-studio-code.png'}
     ]},
-    {id:2, name: 'Loja Zap', img:'../../public/img/projetos/Loja_Zap_shadcn_ui/01.png', link:'https://shadcn-loja-nine.vercel.app/', logo:'../../public/img/projetos/logos/b7web.png', icones:[
+    {id:2, name: 'Loja Zap', img:LojaZap, link:'https://shadcn-loja-nine.vercel.app/', logo: B7web, icones:[
         {id:0, nome:'../../public/img/skills/react.png'},
         {id:1, nome:'../../public/img/skills/tailwindcss.png'},
         {id:2, nome:'../../public/img/skills/webstorm.png'}
--- src/data/SkillsDb.js
+++ src/data/SkillsDb.js
@@ -1,19 +1,36 @@
+import Html from "../../public/img/skills/Html.png"
+import Css from "../../public/img/skills/Css.png"
+import JavaScript from "../../public/img/skills/javascript.png"
+import TypeScript from "../../public/img/skills/typescript.png"
+import BootStrap from "../../public/img/skills/bootstrap.png"
+import React from "../../public/img/skills/react.png"
+import TailWindCss from "../../public/img/skills/tailwindcss.png"
+import Git from "../../public/img/skills/git.png"
+import MySql from "../../public/img/skills/mysql.png"
+import NextJs from "../../public/img/skills/nextjs.png"
+import Linux from "../../public/img/skills/linux.png"
+import Windows from "../../public/img/skills/windows.png"
+import Bash from "../../public/img/skills/terminal.png"
+import Figma from "../../public/img/skills/figma.png"
+import Inkscape from "../../public/img/skills/inkscape.png"
+import Docker from "../../public/img/skills/docker.png"
+
 export const SkillsDb = [
-    {id:0, nome: 'Html', img:'../../public/img/skills/Html.png'},
-    {id:1, nome: 'Css', img:'../../public/img/skills/Css.png'},
-    {id:2, nome: 'JavaScript', img:'../../public/img/skills/javascript.png'},
-    {id:3, nome: 'TypeScript', img:'../../public/img/skills/typescript.png'},
-    {id:4, nome: 'BootStrap', img:'../../public/img/skills/bootstrap.png'},
-    {id:5, nome: 'React', img:'../../public/img/skills/react.png'},
-    {id:6, nome: 'TailWindCss', img:'../../public/img/skills/tailwindcss.png'},
-    {id:7, nome: 'Git', img:'../../public/img/skills/git.png'},
-    {id:8, nome: 'MySql', img:'../../public/img/skills/mysql.png'},
-    {id:9, nome: 'NextJs', img:'../../public/img/skills/nextjs.png'},
-    {id:10, nome: 'Linux', img:'../../public/img/skills/linux.png'},
-    {id:11, nome: 'Windows', img:'../../public/img/skills/windows.png'},
-    {id:12, nome: 'Bash', img:'/../../public/img/skills/terminal.png'},
-    {id:13, nome: 'Figma', img:'../../public/img/skills/figma.png'},
-    {id:14, nome: 'Inkscape', img:'../../public/img/skills/inkscape.png'},
-    {id:15, nome: 'Docker', img:'../../public/img/skills/docker.png'},
+    {id:0, nome: 'Html', img: Html},
+    {id:1, nome: 'Css', img: Css},
+    {id:2, nome: 'JavaScript', img: JavaScript},
+    {id:3, nome: 'TypeScript', img: TypeScript},
+    {id:4, nome: 'BootStrap', img: BootStrap},
+    {id:5, nome: 'React', img: React},
+    {id:6, nome: 'TailWindCss', img: TailWindCss},
+    {id:7, nome: 'Git', img: Git},
+    {id:8, nome: 'MySql', img: MySql},
+    {id:9, nome: 'NextJs', img: NextJs},
+    {id:10, nome: 'Linux', img: Linux},
+    {id:11, nome: 'Windows', img: Windows},
+    {id:12, nome: 'Bash', img: Bash},
+    {id:13, nome: 'Figma', img: Figma},
+    {id:14, nome: 'Inkscape', img: Inkscape},
+    {id:15, nome: 'Docker', img: Docker},
 
 ]
